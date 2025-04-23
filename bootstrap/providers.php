5e9dc1266409e06c8f25c17ea275064e24881666
Sarmad Alkadasi <alkadasi5020@gmail.com>
2025-04-23 20:01:35 +0300

    feat: add VendorPanel
    add VendorPanel for Filament integration, this panal will be use as a dashboard for vendors.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\VendorPanelProvider::class,
     App\Providers\TenancyServiceProvider::class,
 ];
