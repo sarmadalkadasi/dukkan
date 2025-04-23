6d55424ad27cc699bb8761c6b92ee8317ccdc129
Sarmad Alkadasi <alkadasi5020@gmail.com>
2025-04-23 19:48:19 +0300

    install tenancy package for multi tenant

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        // your actual routes
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+        Route::get('/', function () {
+            return Inertia::render('welcome');
+        })->name('home');
+        
+        Route::middleware(['auth', 'verified'])->group(function () {
+            Route::get('dashboard', function () {
+                return Inertia::render('dashboard');
+            })->name('dashboard');
+        });
+        
+        require __DIR__.'/settings.php';
+        require __DIR__.'/auth.php';
+    });
+}
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
