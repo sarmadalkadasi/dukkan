1667f21ee3d3ccc25a77fcee5cfd16df8cd26868
Sarmad Alkadasi <alkadasi5020@gmail.com>
2025-05-22 18:18:29 +0300

    feat: integrate language switch functionality with Filament

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['ar','en']); // also accepts a closure
+        });
         /*Pass domain to front-end in props*/
         Inertia::share([
             'domain' => fn () => request()->getHost(),
