b41becfd3cbefb4d9dff025f31406f1f1789a94b
Sarmad Alkadasi <alkadasi5020@gmail.com>
2025-06-22 17:41:37 +0300

    fix:style by adding daisyui package

--- resources/js/components/store/CartModal.tsx
+++ resources/js/components/store/CartModal.tsx
@@ -8,12 +8,12 @@ const CartModal = () => {
         const {totalQuantity, totalPrice, miniCartItems} = usePage().props;
 
     return (
-        <div className="absolute top-12 right-0 z-20 flex w-max flex-col gap-6 rounded-md bg-white p-4 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className="absolute top-12 right-0 z-20 flex w-max flex-col gap-6 rounded-md bg-background p-4 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
             <h2 className="text-xl">Shopping Cart</h2>
             {/* LIST */}
             <div className="flex flex-col gap-8">
                 {/* ITEM */}
-                <div tabIndex={0} className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-[480px] shadow">
+                <div tabIndex={0} className="card card-compact dropdown-content bg-background z-1 mt-3 w-[480px] shadow">
                     <div className="card-body">
                         <span className="text-lg font-bold">{totalQuantity} Items</span>
 
