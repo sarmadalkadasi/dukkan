ad1273e8b6f060d757d4b01f31908cbc010001c0
Sarmad Alkadasi <alkadasi5020@gmail.com>
2025-06-26 19:08:09 +0300

    fix: uploading logos

--- app/Filament/Vendor/Pages/Settings.php
+++ app/Filament/Vendor/Pages/Settings.php
@@ -2,32 +2,49 @@
 
 namespace App\Filament\Vendor\Pages;
 
+use \Filament\Forms\Components\Textarea;
 use App\Livewire\SubscriptionInformationWidget;
+use App\Models\Tenant;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
 
 class Settings extends Page
 {
+    use InteractsWithForms;
+    
     protected static ?string $navigationIcon = 'heroicon-o-adjustments-horizontal';
 
     protected static ?string $navigationGroup = 'Settings';
 
-    protected static ?string $navigationLabel = 'Profile Settings';
+    protected static ?string $navigationLabel = 'My Settings';
 
     protected static string $view = 'filament.pages.settings';
 
-    public ?array $settings = [];
+    public ?array $data = [];
 
-    public function __construct()
+    public function mount(): void
     {
-        $this->settings = [
-            'name' => auth()->user()->name,
-            'email' => auth()->user()->email,
-        ];
+        $user = auth()->user();
+        $store = Tenant::firstWhere('email', $user->email);
+
+        $this->form->fill([
+            'user' => [
+                'name' => $user->name,
+                'email' => $user->email,
+            ],
+            'store' => [
+                'name' => $store->name,
+                'description' => $store->description,
+            ],
+        ]);
     }
+
 
     protected function getHeaderWidgets(): array
     {
@@ -40,31 +57,52 @@ class Settings extends Page
     {
         return $form
             ->schema([
-                Grid::make(2)->schema([
-                    TextInput::make('name')
-                        ->label('Name')
-                        ->required()
-                        ->maxLength(255),
-                    TextInput::make('email')
-                        ->label('Email')
-                        ->required()
-                        ->email()
+                Section::make('Profile Settings')->schema([
+                    Grid::make(2)->schema([
+                        TextInput::make('user.name')
+                            ->label('Name')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('user.email')
+                            ->label('Email')
+                            ->required()
+                            ->email()
+                            ->maxLength(255)
+                            ->unique(ignorable: auth()->user()),
+                    ]), 
+                ]), 
+                Section::make('Store Settings')->schema([
+                    Grid::make(2)->schema([
+                        TextInput::make('store.name')
+                            ->label('Store Name')
+                            ->maxLength(155)
+                            ->required(),
+                        Textarea::make('store.description')
                         ->maxLength(255)
-                        ->unique(ignorable: auth()->user())
-                ])
+                        ->columnSpanFull(),                     
+
+                    ])
+                ]),
                 
-            ])->statePath('settings');
+            ])->statePath('data');
+
     }
 
     public function save(): void
     {
-        $state = $this->form->getState();
+        $data = $this->form->getState();
+        $user = auth()->user();
 
-        auth()->user()->update([
-            'name' => $state['name'],
-            'email' => $state['email'],
+        $user->update([
+            'name' => $data['user']['name'],
+            'email' => $data['user']['email'],
+        ]);
+        
+        tenant()->update([
+            'name' => $data['store']['name'],
+            'description' => $data['store']['description'],
         ]);
 
-        Notification::make()->title('Profile updated successfully!')->success()->send();
+        Notification::make()->title("It's updated successfully!")->success()->send();
     }
 }
